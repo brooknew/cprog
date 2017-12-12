dc680e1366019aa1b7c39493522344614b60be60
brooknew <brooknew@163.com>
2017-12-12 23:39:34 +0800

    vc6 test program

---
+++ vc6/test1/test1.cpp
@@ -0,0 +1,12 @@
+// test1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+
+int main(int argc, char* argv[])
+{
+	printf("Hello world\n" ) ;
+	return 0;
+}
